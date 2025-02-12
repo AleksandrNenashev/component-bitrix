796b5e592f803f3fd2e1bf4dc3ee92621c12ec6e
Александр Ненашев <nenashev.a@rscable.ru>
2025-02-13 01:26:36 +0400

    add component.php

---
+++ components/custom/user.groups/component.php
@@ -0,0 +1,8 @@
+<?php
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+
+require_once(__DIR__ . "/class.php");
+
+$component = new UserGroupsComponent($this);
+$component->executeComponent();
+?>
