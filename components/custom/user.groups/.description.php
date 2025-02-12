e801b2afc0fe5fc2d99b82e2e3685c54b5992e7a
Александр Ненашев <nenashev.a@rscable.ru>
2025-02-13 01:23:17 +0400

    add description.php

---
+++ components/custom/user.groups/.description.php
@@ -0,0 +1,14 @@
+<?php
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+
+$arComponentDescription = [
+    "NAME" => "Список групп пользователей",
+    "DESCRIPTION" => "Выводит список групп пользователей в виде таблицы",
+    "PATH" => [
+        "ID" => "custom",
+        "NAME" => "Пользовательские компоненты"
+    ],
+    "CACHE_PATH" => "Y",
+    "COMPLEX" => "N"
+];
+?>
